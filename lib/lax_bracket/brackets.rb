65a99f609c3df781334ec5e32083f2d9a2d3095e
Shea Kusiak <shkusiak@gmail.com>
2018-05-26 21:10:16 +0000

    added attr_accessors :name, :teams, :winner, :url

--- lib/lax_bracket/brackets.rb
+++ lib/lax_bracket/brackets.rb
@@ -1,11 +1,14 @@
 #class DailyDeal::Deal
 class LaxBracket::Brackets
+  attr_accessor :name, :teams, :winner, :url
+
   def self.all
     #I should return a bunch of insances of brackets
     puts <<-DOC.gsub /^\s*/, ''
       Bracket #1:
         1. Trinity
-        2. etc.
+        2. Middlebury
+        3. Salisbury
       Bracket #2:
         1. Middlebury
         2. etc.
@@ -16,5 +19,20 @@ class LaxBracket::Brackets
         1. Amherst
         2. etc.
     DOC
+    bracket_1 = self.new
+    bracket_1.name = "Bracket #1"
+    bracket_1.teams = ["Trinity", "Springfield", "Salisbury"]
+    bracket_1.winner = "Salisbury"
+    bracket_1.url = "https://www.ncaa.com/interactive-bracket/lacrosse-women/d3"
+
+    bracket_2 = self.new
+    bracket_2.name = "Bracket #2"
+    bracket_2.teams = ["Tufts", "Bowdoin", "TCNJ"]
+    bracket_2.winner = "TCNJ"
+    bracket_2.url = "https://www.ncaa.com/interactive-bracket/lacrosse-women/d3"
+
+    [bracket_1, bracket_2]
   end
+
+
 end
