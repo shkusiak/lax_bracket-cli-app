f269915eede364fb0416af258286767e77bc1a9e
Shea Kusiak <shkusiak@gmail.com>
2018-05-26 22:44:02 +0000

    Added .scrape_brackets

--- lib/lax_bracket/brackets.rb
+++ lib/lax_bracket/brackets.rb
@@ -5,20 +5,29 @@ class LaxBracket::Brackets
   def self.all
     # Scrape ncaa and return brackets based on that data
 
-    bracket_1 = self.new
-    bracket_1.name = "Bracket #1"
-    bracket_1.teams = ["Trinity", "Springfield", "Salisbury"]
-    bracket_1.winner = "Salisbury"
-    bracket_1.url = "https://www.ncaa.com/interactive-bracket/lacrosse-women/d3"
-
-    bracket_2 = self.new
-    bracket_2.name = "Bracket #2"
-    bracket_2.teams = ["Tufts", "Bowdoin", "TCNJ"]
-    bracket_2.winner = "TCNJ"
-    bracket_2.url = "https://www.ncaa.com/interactive-bracket/lacrosse-women/d3"
-
-    [bracket_1, bracket_2]
+    self.scrape_brackets
   end
 
+  def self.scrape_brackets
+    brackets = []
+    #go to NCAA, find the brackets,
+    #extract the properties
+    #initiate a bracket
+
+    # bracket_1 = self.new
+    # bracket_1.name = "Bracket #1"
+    # bracket_1.teams = ["Trinity", "Springfield", "Salisbury"]
+    # bracket_1.winner = "Salisbury"
+    # bracket_1.url = "https://www.ncaa.com/interactive-bracket/lacrosse-women/d3"
+    #
+    # bracket_2 = self.new
+    # bracket_2.name = "Bracket #2"
+    # bracket_2.teams = ["Tufts", "Bowdoin", "TCNJ"]
+    # bracket_2.winner = "TCNJ"
+    # bracket_2.url = "https://www.ncaa.com/interactive-bracket/lacrosse-women/d3"
+    #
+    # [bracket_1, bracket_2]
+    brackets
+  end
 
 end
