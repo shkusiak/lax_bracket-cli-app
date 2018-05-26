3663904092f1bb25ddd9dc1314b356a117ecd205
Shea Kusiak <shkusiak@gmail.com>
2018-05-26 21:26:47 +0000

    modified .all

--- lib/lax_bracket/brackets.rb
+++ lib/lax_bracket/brackets.rb
@@ -4,21 +4,13 @@ class LaxBracket::Brackets
 
   def self.all
     #I should return a bunch of insances of brackets
-    puts <<-DOC.gsub /^\s*/, ''
-      Bracket #1:
-        1. Trinity
-        2. Middlebury
-        3. Salisbury
-      Bracket #2:
-        1. Middlebury
-        2. etc.
-      Bracket #3:
-        1. Salisbury
-        2. etc.
-      Bracket #4:
-        1. Amherst
-        2. etc.
-    DOC
+    # puts <<-DOC.gsub /^\s*/, ''
+    #   Bracket #1:
+    #     1. Trinity
+    #     2. Middlebury
+    #     3. Salisbury
+    #   Bracket #2...
+    # DOC
     bracket_1 = self.new
     bracket_1.name = "Bracket #1"
     bracket_1.teams = ["Trinity", "Springfield", "Salisbury"]
