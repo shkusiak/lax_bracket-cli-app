b8bdcfe4d1d26bf974e67afe6985d29c70af5f88
Shea Kusiak <shkusiak@gmail.com>
2018-05-27 23:58:49 +0000

    building bracket #1 hash

--- lib/lax_bracket/brackets.rb
+++ lib/lax_bracket/brackets.rb
@@ -1,6 +1,6 @@
 #class DailyDeal::Deal
 class LaxBracket::Brackets
-  attr_accessor :name, :teams, :winner, :url
+  attr_accessor :name, :teams, :winner, :url, :games
 
   def self.all
     # Scrape ncaa and return brackets based on that data
@@ -14,6 +14,11 @@ class LaxBracket::Brackets
     #extract the properties
     #initiate a bracket
     @brackets << self.scrape_ncaa
+    @brackets << self.scrape_ncaa
+
+    @brackets << self.scrape_ncaa
+    @brackets << self.scrape_ncaa
+
     # bracket_1 = self.new
     # bracket_1.name = "Bracket #1"
     # bracket_1.teams = ["Trinity", "Springfield", "Salisbury"]
@@ -29,9 +34,20 @@ class LaxBracket::Brackets
     bracket.name = "Bracket ##{@brackets.length + 1}"
     #doc.search("h1.element-invisible").text
     bracket.teams = ["team 1", "team 2", "team 3"]
+    if bracket.name = "Bracket #1"
+      bracket.games = []
+      doc.search("#bracket-top-left h3.element-invisible").each do |game|
+
+        game_teams = game.text
+        game_score = "9 to 10"
+        game_round = "round #"
+        bracket.games << {game_round => [game_teams, game_score]}
+
+      end
+    end
     bracket.winner = doc.search("h2").text
     bracket.url = doc.search("h2").first.attr("href")
     bracket
-    #binding.pry
+    binding.pry
   end
 end
