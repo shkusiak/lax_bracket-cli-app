1c5c60c9840089a084321a617e4e6d8413fa5c99
Shea Kusiak <shkusiak@gmail.com>
2018-06-01 00:36:35 +0000

    broken scaper for team name & rank

--- lib/lax_bracket/original_rank.rb
+++ lib/lax_bracket/original_rank.rb
@@ -2,42 +2,30 @@ class LaxBracket::OriginalRank
   attr_accessor :name, :rank
 
   def self.all
-    self.scrape_original_ranks
+    self.scrape_ncaa_ranks
   end
 
   def self.scrape_original_ranks
     @original_ranks = []
-    #go to https://www.ncaa.com/rankings/lacrosse-women/d3/iwlca-coaches
-    #extract the properties
-    #initiate the list
-    #print the list
-
+    #go to https://www.ncaa.com/rankings/lacrosse-women/d3/iwlca-coaches, extract the properties, #initiate the list, #print the list
     @original_ranks << self.scrape_ncaa_ranks
     @original_ranks
-    # @original_ranks.each.with_index(1) do |team, i|
-    #   puts "#{i}. #{team}"
-    # end
-
   end
 
   def self.scrape_ncaa_ranks
     doc = Nokogiri::HTML(open('https://www.ncaa.com/rankings/lacrosse-women/d3/iwlca-coaches'))
-
+    @teams = []
     team = self.new
-    binding.pry
-    doc.search("td").each do |team_name|
-      team.name = team_name.text
-      team.rank = 1
+
+    #binding.pry
+
+    doc.xpath("//tr").each do |team_info|
+      team.name = team_info.css("td")[1]
+      team.rank = team_info.css("td")[0]
+      @teams << team
     end
 
-    #team.name = doc.search("td")[1].text
-    #team.rank = 1
-    # doc.search("rankings-content tbody tr").each.with_index(1) do |team_name, i|
-    #   team.rank = i
-    #   team.name = team_name
-    # end
-
-    team
+    @teams
 
   end
 
