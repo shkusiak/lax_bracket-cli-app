8ee2bb1ef4293c88059d913435cb3fe66aaeb408
Shea Kusiak <shkusiak@gmail.com>
2018-06-03 21:27:49 +0000

    updated outline of cli.rb to match NOTES.md

--- lib/lax_bracket/cli.rb
+++ lib/lax_bracket/cli.rb
@@ -4,17 +4,17 @@
 class LaxBracket::CLI
 
   def call
-    list_original_rank
+    list_pre_rank
   #  list_brackets
     menu
     goodbye
   end
 
-  def list_original_rank
+  def list_pre_rank
     puts "Prior to playoffs, the rankings were:"
-    @original_ranks = LaxBracket::OriginalRank.all
-    @original_ranks
-    @original_ranks.each do |team|
+    @pre_ranks = LaxBracket::OriginalRank.all
+
+    @pre_ranks.each do |team|
       puts "#{team.rank}. #{team.name}"
     end
   end
@@ -33,7 +33,7 @@ class LaxBracket::CLI
   def menu
     input = nil
     while input != "exit"
-      puts "Enter the number of the bracket you would like more info on or type list to see the brackets again or type exit:"
+      puts "Enter the number of the team you would like more info on or type 'brackets', 'results', 'pre rankings' or 'exit':"
       input = gets.strip.downcase
 
       if input.to_i > 0
@@ -42,15 +42,28 @@ class LaxBracket::CLI
         the_bracket.teams.each.with_index(1) do |team, i|
           puts "#{i}. #{team}"
         end
-      elsif input == "list"
+      elsif input == "brackets"
         list_brackets
+      elsif input == "pre rankings"
+        list_pre_rank
+      elsif input == "results"
+        list_results
+      elsif input == "exit"
       else
-        puts "Not sure what you want, type list or exit."
+        puts "Not sure what you want, type 'brackets', 'results', or 'exit'."
       end
     end
   end
 
+  def follow_team
+    puts "now you are following a team"
+  end
+
+  def list_results
+    puts "here are the results"
+  end
+
   def goodbye
-    puts "See you tomorrow for more deals!!!"
+    puts "type './bin/lax-bracket' to run the program again."
   end
 end
--- lib/lax_bracket/original_rank.rb
+++ lib/lax_bracket/original_rank.rb
@@ -5,12 +5,12 @@ class LaxBracket::OriginalRank
     self.scrape_ncaa_ranks
   end
 
-  def self.scrape_original_ranks
-    @original_ranks = []
-    #go to https://www.ncaa.com/rankings/lacrosse-women/d3/iwlca-coaches, extract the properties, #initiate the list, #print the list
-    @original_ranks << self.scrape_ncaa_ranks
-    @original_ranks
-  end
+  # def self.scrape_original_ranks
+  #   @original_ranks = []
+  #   #go to https://www.ncaa.com/rankings/lacrosse-women/d3/iwlca-coaches, extract the properties, #initiate the list, #print the list
+  #   @original_ranks << self.scrape_ncaa_ranks
+  #   @original_ranks
+  # end
 
   def self.scrape_ncaa_ranks
     doc = Nokogiri::HTML(open('https://www.ncaa.com/rankings/lacrosse-women/d3/iwlca-coaches'))
@@ -19,9 +19,9 @@ class LaxBracket::OriginalRank
 
     #binding.pry
 
-    doc.xpath("//tr").each do |team_info|
-      team.name = team_info.css("td")[1]
-      team.rank = team_info.css("td")[0]
+    doc.search("tbody").each do |team_info|
+      team.name = team_info.css("tr td")[1].text
+      team.rank = team_info.css("tr td")[0].text
       @teams << team
     end
 
