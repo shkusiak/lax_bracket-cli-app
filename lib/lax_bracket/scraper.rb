a970cf3a6d35e90e429196b68af669fe63876f41
Shea Kusiak <shkusiak@gmail.com>
2018-06-22 22:56:22 +0000

    removed white space

--- lib/lax_bracket/scraper.rb
+++ lib/lax_bracket/scraper.rb
@@ -3,7 +3,6 @@ class LaxBracket::Scraper
   @@games = []
   @@pre_ranks = []
 
-
   def self.scrape_games
     doc = Nokogiri::HTML(open('https://www.ncaa.com/interactive-bracket/lacrosse-women/d3'))
 
